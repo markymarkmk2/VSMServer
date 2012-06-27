006bc8ec675b6050aaa4b87ba7c0c9288f9b32c3
Mark Williams <mark@dimm.de>
2012-06-27 17:20:27 +0000

    Calc Statistics

--- src/de/dimm/vsm/fsengine/PoolMapper.java
+++ src/de/dimm/vsm/fsengine/PoolMapper.java
@@ -5,8 +5,14 @@
 
 package de.dimm.vsm.fsengine;
 
+import de.dimm.vsm.Utilities.SizeStr;
+import de.dimm.vsm.log.Log;
 import de.dimm.vsm.records.StoragePool;
 import de.dimm.vsm.records.StoragePoolNub;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.persistence.EntityManagerFactory;
 
 /**
@@ -23,6 +29,8 @@ public class PoolMapper
     FSEIndexer indexer;
     HashCache hashCache;
 
+
+
     public PoolMapper( StoragePool pool, StoragePoolNub nub, MiniConnectionPoolManager poolManager, JDBCEntityManager em, EntityManagerFactory poolEmf, FSEIndexer indexer )
     {
         this.pool = pool;
@@ -33,6 +41,118 @@ public class PoolMapper
         this.indexer = indexer;
         hashCache = new HashCache(pool);
     }
+
+    public void calcStats()
+    {
+        try
+        {
+            calcStats(em.getConnection());
+        }
+        catch (SQLException sQLException)
+        {
+        }
+    }
+    long dedupDataLen = 0;
+    long actFsDataLen = 0;
+    long actFileCnt = 0;
+    long totalFsDataLen = 0;
+    long totalFileCnt = 0;
+
+    int getActDedupRatio()
+    {
+        int ratio = 0;
+        if (dedupDataLen > 0 && totalFsDataLen > 0 && actFsDataLen > 0)
+        {
+            ratio = 100 - (int)(100 * dedupDataLen / actFsDataLen + 0.5);
+        }
+        return ratio;
+    }
+    int getTotalDedupRatio()
+    {
+        int ratio = 0;
+        if (dedupDataLen > 0 && totalFsDataLen > 0 && actFsDataLen > 0)
+        {
+            ratio = 100 - (int)(100 * dedupDataLen / totalFsDataLen + 0.5);
+        }
+        return ratio;
+    }
+
+    public void logStats()
+    {
+        Log.info("Block-Datenmenge für Pool", pool.getName() + ": " + SizeStr.format(dedupDataLen));
+        Log.info("Virtuelle aktuelle  Dateisystemgröße für Pool", pool.getName() + ": " + SizeStr.format(actFsDataLen) + " bei " + actFileCnt + " Elementen");
+        Log.info("Virtuelle komplette Dateisystemgröße für Pool", pool.getName() + ": " + SizeStr.format(totalFsDataLen) + " bei " + totalFileCnt + " Elementen");
+        Log.info("Dedupfaktor aktuell  für Pool", pool.getName() + ": " + getActDedupRatio() + "%%");
+        Log.info("Dedupfaktor komplett für Pool", pool.getName() + ": " + getTotalDedupRatio() + "%%");
+    }
+    void calcStats( Connection conn)
+    {
+        Statement st  = null;
+        try
+        {
+
+            st = conn.createStatement();
+
+            ResultSet rs = st.executeQuery("select sum(bigint(blockLen)) from DedupHashBlock");
+
+
+            if (rs.next())
+            {
+                dedupDataLen = rs.getLong(1);
+            }
+            rs.close();
+
+            rs = st.executeQuery("select count(*), sum(bigint(a.fsize)), sum(bigint(a.xasize)) from FileSystemElemAttributes a, FileSystemElemNode f where f.attributes_idx=a.idx");
+
+            if (rs.next())
+            {
+                actFileCnt = rs.getLong(1);
+                actFsDataLen = rs.getLong(2) + rs.getLong(3);
+            }
+            rs.close();
+
+            rs = st.executeQuery("select count(*), sum(bigint(a.fsize)), sum(bigint(a.xasize)) from FileSystemElemAttributes a");
+
+            if (rs.next())
+            {
+                totalFileCnt = rs.getLong(1);
+                totalFsDataLen = rs.getLong(2) + rs.getLong(3);
+            }
+            rs.close();
+
+
+            
+        }
+        catch (SQLException sQLException)
+        {
+            Log.err("Statistik kann nicht gerechnet werden", pool.getName(), sQLException);
+        }
+        finally
+        {
+            if (st != null)
+            {
+                try
+                {
+                    st.close();
+                }
+                catch (SQLException sQLException)
+                {
+                }
+            }
+
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch (SQLException sQLException)
+                {
+                }
+            }
+        }
+    }
+
 
 
 }
