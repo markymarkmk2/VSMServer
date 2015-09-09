776aa567ad8ea98471951ab6635cc772a7333476
Mark Williams <mark@dimm.de>
2015-09-09 19:21:13 +0000

    V1.8.5: Fix für falsches Attribut in getNodeLen

--- src/de/dimm/vsm/Main.java
+++ src/de/dimm/vsm/Main.java
@@ -55,7 +55,7 @@ public class Main
 {
 
     static String source_str = "trunk";
-    static String version_str = "1.8.4";
+    static String version_str = "1.8.5";
         
     public static int writeThreads = 1;
     public static int maxOpenFiles = 1024;
